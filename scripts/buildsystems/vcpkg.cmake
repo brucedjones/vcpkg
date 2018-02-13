106981d537df76d6e704b15519c8103d2812dd9f
Robert Schumacher <roschuma@microsoft.com>
2018-02-13 01:48:42 -0800

    [vcpkg-cmake-toolchain] Add VCPKG_APPLOCAL_LIBRARY_DEPS to enable applocal deployment for DLLs.

--- scripts/buildsystems/vcpkg.cmake
+++ scripts/buildsystems/vcpkg.cmake
@@ -139,6 +139,7 @@ foreach(_VCPKG_TOOLS_DIR ${_VCPKG_TOOLS_DIRS})
 endforeach()
 
 option(VCPKG_APPLOCAL_DEPS "Automatically copy dependencies into the output directory for executables." ON)
+option(VCPKG_APPLOCAL_LIBRARY_DEPS "Automatically copy dependencies into the output directory for libraries." OFF)
 function(add_executable name)
     _add_executable(${ARGV})
     list(FIND ARGV "IMPORTED" IMPORTED_IDX)
@@ -163,6 +164,15 @@ function(add_library name)
     list(FIND ARGV "INTERFACE" INTERFACE_IDX)
     list(FIND ARGV "ALIAS" ALIAS_IDX)
     if(IMPORTED_IDX EQUAL -1 AND INTERFACE_IDX EQUAL -1 AND ALIAS_IDX EQUAL -1)
+        get_target_property(_is_library_shared ${name} TYPE)
+        if(VCPKG_APPLOCAL_LIBRARY_DEPS AND _VCPKG_TARGET_TRIPLET_PLAT MATCHES "windows|uwp" AND _is_library_shared STREQUAL "SHARED_LIBRARY")
+            add_custom_command(TARGET ${name} POST_BUILD
+                COMMAND powershell -noprofile -executionpolicy Bypass -file ${_VCPKG_TOOLCHAIN_DIR}/msbuild/applocal.ps1
+                    -targetBinary $<TARGET_FILE:${name}>
+                    -installedDir "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}$<$<CONFIG:Debug>:/debug>/bin"
+                    -OutVariable out
+            )
+        endif()
         set_target_properties(${name} PROPERTIES VS_USER_PROPS do_not_import_user.props)
         set_target_properties(${name} PROPERTIES VS_GLOBAL_VcpkgEnabled false)
     endif()
